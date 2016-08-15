36bc092c6109dfa954bf53dd6caee8a3407bb944
Vikas Kumar <vrnvikas1994@gmail.com>
2016-08-15 17:57:24 +0530

    Initial commit

---
+++ app/src/main/java/com/Artoolkit/demo/UnityPlayerActivity.java
@@ -0,0 +1,103 @@
+package com.Artoolkit.demo;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.widget.FrameLayout;
+
+import com.unity3d.player.UnityPlayer;
+
+public class UnityPlayerActivity extends Activity {
+    protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
+
+    // Setup activity layout
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFormat(PixelFormat.RGBX_8888); // <--- This makes xperia play happy
+
+        mUnityPlayer = new UnityPlayer(this);
+        int glesMode = mUnityPlayer.getSettings().getInt("gles_mode", 1);
+        boolean trueColor = false;
+        mUnityPlayer.init(glesMode, trueColor);
+
+        setContentView(R.layout.screen);
+        FrameLayout layout = (FrameLayout) findViewById(R.id.container);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.FILL_PARENT,
+                FrameLayout.LayoutParams.FILL_PARENT);
+        layout.addView(mUnityPlayer.getView(), 0, lp);
+        mUnityPlayer.requestFocus();
+
+    }
+
+    // Quit Unity
+    @Override
+    protected void onDestroy() {
+        mUnityPlayer.quit();
+        super.onDestroy();
+    }
+
+    // Pause Unity
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mUnityPlayer.pause();
+    }
+
+    // Resume Unity
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mUnityPlayer.resume();
+    }
+
+    // This ensures the layout will be correct.
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mUnityPlayer.configurationChanged(newConfig);
+    }
+
+    // Notify Unity of the focus change.
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        mUnityPlayer.windowFocusChanged(hasFocus);
+    }
+
+    // For some reason the multiple keyevent type is not supported by the ndk.
+    // Force event injection by overriding dispatchKeyEvent().
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
+            return mUnityPlayer.injectEvent(event);
+        return super.dispatchKeyEvent(event);
+    }
+
+    // Pass any events not handled by (unfocused) views straight to UnityPlayer
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        return mUnityPlayer.injectEvent(event);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return mUnityPlayer.injectEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return mUnityPlayer.injectEvent(event);
+    }
+
+    /*API12*/
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        return mUnityPlayer.injectEvent(event);
+    }
+}
